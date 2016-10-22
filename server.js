42a77c40c454313eae5539e8791c33feace6f0db
Iswan Jumat <iswan@thinkcube.com>
2016-10-23 00:56:48 +0530

    removed line

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ var serveStatic = require('serve-static');
 
 function shouldCompress(req, res) {
   if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header 
+    // don't compress responses with this request header
     return false
   }
- 
-  // fallback to standard filter function 
+  // fallback to standard filter function
   return compression.filter(req, res)
 }
 
