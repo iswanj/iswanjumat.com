85955acccd91f9743dad1ca3bacec32674b4a6b5
root <root@iswanjumat.com>
2016-10-22 19:12:30 +0000

    added compression

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var connect = require('connect');
+var compression = require('compression');
 var serveStatic = require('serve-static');
+
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header 
+    return false
+  }
+ 
+  // fallback to standard filter function 
+  return compression.filter(req, res)
+}
+
+connect().use(compression({filter: shouldCompress}));
 connect().use(serveStatic(__dirname + '/build')).listen(3000, function(){
     console.log('Server running on 3000...');
 });
