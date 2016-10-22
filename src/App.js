bf8f4246a8c6e3ceaf2996e40e15150b3695b0d5
Iswan Jumat <iswan@thinkcube.com>
2016-10-22 23:37:59 +0530

    finished website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
+import './font-awesome.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="rightBox">
+          <div className="myImage"></div>
+          <div className="position">
+            <p>Frontend Developer</p>
+            <h1>Iswan Jumat</h1>
+          </div>
+          <div className="social-icons">
+            <ul>
+              <li><a href="https://twitter.com/TuanIswan" className="twitter" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.facebook.com/iswan1987" className="facebook" target="_blank"><i className="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+              <li><a href="https://lk.linkedin.com/in/iswanjumat" className="linkedin" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+              <li><a href="https://github.com/iswanj" className="github" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
