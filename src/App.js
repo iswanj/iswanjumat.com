a2317691eeb0f0ef12933c9dd526fd070d139a63
Iswan Jumat <info@iswanjumat.com>
2020-08-03 11:15:44 +0800

    updated dependancy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./reset.css";
+import "./App.css";
+import "./font-awesome.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    appStyle: null,
+  };
+  componentDidMount() {
+    const images = [0, 1, 2, 3, 4, 5];
+    let randImage = images[Math.floor(Math.random() * images.length)];
+    const newBackImage = new Image();
+    newBackImage.src = `${randImage}.jpg`;
+    newBackImage.onload = () => {
+      this.setState({
+        appStyle: {
+          backgroundImage: `url(${newBackImage.src})`,
+          display: "flex",
+        },
+      });
+    };
+  }
+  render() {
+    const { appStyle } = this.state;
+    return (
+      <div className="App" style={appStyle}>
+        <div className="rightBox">
+          <div className="myImage"></div>
+          <div className="position">
+            <p>Frontend Developer</p>
+            <h1>Iswan Jumat</h1>
+          </div>
+          <div className="social-icons">
+            <ul>
+              <li>
+                <a
+                  href="https://twitter.com/TuanIswan"
+                  className="twitter"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://lk.linkedin.com/in/iswanjumat"
+                  className="linkedin"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/iswanj"
+                  className="github"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/iswanjumat/"
+                  className="instagram"
+                  target="noopener noreferrer"
+                  rel="noopener"
+                >
+                  <i className="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
